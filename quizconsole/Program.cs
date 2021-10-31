b3700b71a714ba373f8832a83eee01b74f3fecad
Kacpersiekacz <kahty11@gmail.com>
2021-10-31 12:50:45 +0100

    quiz

---
+++ quizconsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace quizconsole
+{
+    class Pytanie
+    {
+        int ;
+        Pytanie pytanie1;
+        Pytanie pytanie2;
+        Pytanie pytanie3;
+        Pytanie pytanie4;
+        Pytanie pytanie5;
+
+        static void Main(string[]   )
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+    }
+
+}
